02e6693004110c980a1de92ac6c323d5c0e6fd62
Sidney Andrews <sidney@seesharprun.net>
2017-02-02 16:33:05 -0500

    Added .NET Project

---
+++ Controllers/ShippingDataController.cs
@@ -0,0 +1,16 @@
+﻿using Airal.Shipping.Models;
+using Swashbuckle.Swagger.Annotations;
+using System.Web.Http;
+
+namespace Airal.Shipping.Controllers
+{
+    public class ShippingDataController : ApiController
+    {
+        // GET api/shippingdata
+        [SwaggerOperation("GetShippingData")]
+        public ShippingData Get()
+        {
+            return ShippingDataFactory.GetData();
+        }
+    }
+}
